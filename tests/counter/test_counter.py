0a534e92a875c8622796268780119f3ce87ca3b5
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-09 17:23:12 -0300

    feat: requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    response = count_ocurrences("src/jobs.csv", "National")
+    assert response == 2140
+    response = count_ocurrences("src/jobs.csv", "SomeWord")
+    assert response == 0
